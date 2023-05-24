d4ff1f20709316be3952da83ac20560e4e8f4fc1
tieas angraeni <tieasnura@gmail.com>
2023-05-24 16:00:39 +0700

    login view

--- app/src/main/java/com/tisco/aquaapitest/MainActivity.java
+++ app/src/main/java/com/tisco/aquaapitest/MainActivity.java
@@ -3,12 +3,21 @@ package com.tisco.aquaapitest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email_login);
+        pass = findViewById(R.id.pass_login);
+
+
+
     }
 }
